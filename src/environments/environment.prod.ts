faefa3573e8e1cc7bd27daf07cf097217631da5d
Benoît E <944741+beneccli@users.noreply.github.com>
2022-04-16 16:54:53 +0200

    Fix prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://www.hongkong-rent-map.tk/.netlify/functions',
+  apiUrl: 'https://www.hongkong-rent-map.tk/.netlify/functions',
 };
