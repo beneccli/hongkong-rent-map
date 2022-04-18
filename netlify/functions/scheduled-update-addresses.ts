bd14103e52e5279935b5bcbc8970b9d25d2f90f5
Benoît E <944741+beneccli@users.noreply.github.com>
2022-04-18 18:34:47 +0200

    Add scheduled updates of addresses and coordinates

---
+++ netlify/functions/scheduled-update-addresses.ts
@@ -0,0 +1,41 @@
+import { schedule } from '@netlify/functions';
+import * as airtable from 'airtable';
+import { AirtableBase } from 'airtable/lib/airtable_base';
+import { getBaseIdByLocation } from "netlify/utils/get-base-by-location";
+import { getSteps } from "netlify/utils/get-step";
+import { updateAddresses } from 'netlify/utils/update-addresses';
+
+const updateStatus = async (base: AirtableBase, schedulingStatus: any): Promise<void> => {
+  const status = schedulingStatus.fields.status + 1;
+  base('scheduling').update([{
+    id: schedulingStatus.getId(),
+    fields: { status: status === getSteps.length ? 0 : status }
+  }]);
+}
+
+export const handler = schedule(
+  "*/10 * * * *",
+  async () => {
+    const apiKey = 'key7n6E71OR94Ur7a';
+    airtable.configure({ apiKey });
+    const base = airtable.base(getBaseIdByLocation['kw']);
+
+    const schedulingStatus = (await base('scheduling')
+      .select({
+        filterByFormula: `{Name} = 'update-addresses'`
+      }).all())[0];
+    const currentStatus: number = schedulingStatus.fields.status;
+    const currentStep = getSteps[currentStatus];
+    updateStatus(base, schedulingStatus);
+
+    await updateAddresses(
+      currentStep.location,
+      currentStep.lowPrice,
+      currentStep.highPrice
+    );
+
+    return {
+      statusCode: 200,
+    };
+  }
+);
