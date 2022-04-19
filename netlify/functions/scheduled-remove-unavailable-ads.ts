cbc8f18ed6407184ca4a3c8f57d57102c42430ed
Benoît E <944741+beneccli@users.noreply.github.com>
2022-04-19 09:54:07 +0200

    Update remove unavailable ads scheduling cron

--- netlify/functions/scheduled-remove-unavailable-ads.ts
+++ netlify/functions/scheduled-remove-unavailable-ads.ts
@@ -2,7 +2,7 @@ import { schedule } from '@netlify/functions';
 import { removeUnavailableAds } from 'netlify/utils/remove-unavailable-ads';
 
 export const handler = schedule(
-  "@hourly",
+  "*/3 * * * *",
   async () => {
     await removeUnavailableAds('hk');
     await removeUnavailableAds('kw');
