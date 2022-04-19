82ccce4c0f944aad02510ea9f994a2ca4c2170f0
Benoît E <944741+beneccli@users.noreply.github.com>
2022-04-19 19:37:42 +0200

    Add marker grouping

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -2,8 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { environment } from '@env/environment';
-import { faRulerCombined, faTrainSubway, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faHashtag, faRulerCombined, faTrainSubway, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { BehaviorSubject, catchError, of } from 'rxjs';
+
+enum MarkerType {
+  Ad,
+  Group,
+}
+
+interface IMarker {
+  position: google.maps.LatLngLiteral;
+  type: MarkerType;
+  data: any;
+};
 
 @Component({
   selector: 'app-map',
@@ -26,6 +37,8 @@ export class MapComponent {
   faTrainSubway = faTrainSubway;
   faRulerCombined = faRulerCombined;
   faUpRightFromSquare = faUpRightFromSquare;
+  faHashtag = faHashtag;
+  MarkerType = MarkerType;
 
   private _priceRange = '15000-20000';
   private _location = 'hk';
@@ -34,7 +47,7 @@ export class MapComponent {
   public records = new BehaviorSubject<any>([]);
   public markerOptions: google.maps.MarkerOptions = {draggable: false, clickable: true};
   public markerPositions: google.maps.LatLngLiteral[] = [];
-  public markers: {position: google.maps.LatLngLiteral, url: string, price: string | null | undefined}[] = [];
+  public markers: IMarker[] = [];
   public currentMarker: any;
   private mapStyle: any = window?.matchMedia('(prefers-color-scheme: dark)').matches ? [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":"-100"},{"lightness":"30"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"gamma":"0.00"},{"lightness":"74"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"3"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}] : undefined;
   public options: google.maps.MapOptions = {
@@ -47,25 +60,47 @@ export class MapComponent {
     red: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650051640/samples/red-rect_bny1ch.png',
     orange: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650051640/samples/orange-rect_muv5d8.png',
     yellow: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650051640/samples/yellow-rect_j7pc4q.png',
-    green: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650051640/samples/green-rect_q4j5qe.png'
+    green: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650051640/samples/green-rect_q4j5qe.png',
+    gray: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650385555/samples/gray-rect_rzai8x.png',
+    black: 'https://res.cloudinary.com/dmhcgihwc/image/upload/v1650385555/samples/black-rect_taftow.png'
   };
 
   constructor(private http: HttpClient) {
     this.records
       .subscribe((records) => {
-        this.markers = records
-          .filter((record: any) => record.coordinates)
-          .map((record: any) => {
-            return {
-              url: record.detailUrl,
-              position: {lat: record.coordinates.lat, lng: record.coordinates.lon},
-              ...record
-            };
-            // return L
-            //   .marker([record.coordinates.lat, record.coordinates.lon], { icon: this.generateIcon(record.price) })
-            //   .on('mousedown', () => window?.open(record.detailUrl, '_blank')?.focus());
-          })
+        this.markers = this.groupMarkersWithSamePosition(records);
       });
+  }
+
+  private groupMarkersWithSamePosition(records: any[]) {
+    return records
+      .filter((record: any) => record.coordinates)
+      .map((record: any) => {
+        return {
+          position: {lat: record.coordinates.lat, lng: record.coordinates.lon},
+          type: MarkerType.Ad,
+          data: record
+        };
+      })
+      .reduce((acc: any[], record: any) => {
+        const index: number = acc.findIndex((e) =>
+          e.position.lat === record.position.lat &&
+          e.position.lng === record.position.lng
+        );
+
+        if (index >= 0) {
+          if (acc[index].type === MarkerType.Ad) {
+            acc[index].type = MarkerType.Group;
+            acc[index].data = [acc[index].data, record.data];
+          } else {
+            acc[index].data = [...acc[index].data, record.data];
+          }
+        } else {
+          acc = [...acc, record];
+        }
+
+        return acc;
+      }, []);
   }
 
   openInfoWindow(mapMarker: MapMarker, marker: any) {
@@ -73,7 +108,13 @@ export class MapComponent {
     this.infoWindow?.open(mapMarker);
   }
 
-  public transformPrice(price: any) {
+  public findLabel(marker: IMarker): string {
+    return marker.type === MarkerType.Group
+      ? marker.data.length.toString()
+      : this.transformPrice(marker.data.price);
+  }
+
+  public transformPrice(price: number): string {
     const text = price?.toString();
     return isNaN(+text) ? text : (parseInt(text)/1000).toString().substring(0,4) + 'K';
   }
@@ -110,19 +151,25 @@ export class MapComponent {
   }
 
   public findMarkerIcon(marker: any) {
-    if (marker.workTravelTime < 20*60) {
+    if (marker.type === MarkerType.Group) {
+      return this.icons.black;
+    } else if (marker.data.workTravelTime < 20*60) {
       return this.icons.green;
-    } else if (marker.workTravelTime < 40*60) {
+    } else if (marker.data.workTravelTime <= 40*60) {
       return this.icons.yellow;
-    } else if (marker.workTravelTime < 60*60) {
+    } else if (marker.data.workTravelTime <= 60*60) {
       return this.icons.orange;
-    } else {
+    } else if (marker.data.workTravelTime > 60*60) {
       return this.icons.red;
+    } else {
+      return this.icons.gray;
     }
   }
 
   public findMarkerColor(marker: any) {
-    if (marker.workTravelTime < 40*60) {
+    if (marker.type === MarkerType.Group) {
+      return 'white';
+    } else if (marker.data.workTravelTime < 40*60) {
       return 'black';
     } else {
       return 'white';
@@ -133,13 +180,13 @@ export class MapComponent {
     const today: Date = new Date();
     today.setUTCHours(0, 0, 0, 0);
     const threeDaysAgoTimestamp: number = today.getTime() - (3*24*60*60*1000);
-    const markerDateTimestamp: number = new Date(marker.createdDate).getTime();
+    const markerDateTimestamp: number = new Date(marker.data.createdDate).getTime();
     return markerDateTimestamp > threeDaysAgoTimestamp;
   }
 
   public diffDate(marker: any): number {
     const todayTimestamp: number = (new Date()).getTime();
-    const markerDateTimestamp: number = new Date(marker.createdDate).getTime();
+    const markerDateTimestamp: number = new Date(marker.data.createdDate).getTime();
     const diffTimestamp = todayTimestamp - markerDateTimestamp;
     return Math.ceil(diffTimestamp/86400000);
   }
